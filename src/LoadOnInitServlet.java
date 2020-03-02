16b5a9a99aca8fdaf284c86ffe813971896383c3
mallory94 <malloga@hotmail.fr>
2020-03-02 15:32:25 +0100

    ajout d'une servLet initialisant mediathequeData

---
+++ src/LoadOnInitServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+
+@WebServlet (urlPatterns="/initializeResources", loadOnStartup=1)
+public class LoadOnInitServlet extends HttpServlet {
+
+
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		super.init(arg0);
+		try {
+			Class.forName("persistance.MediathequeData");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println("******************************************************************");
+	}
+
+}
