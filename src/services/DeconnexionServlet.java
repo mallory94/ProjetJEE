33554762eac3c9a02ac155120f7880376950d0ae
mallory94 <malloga@hotmail.fr>
2020-03-21 16:32:32 +0100

    ajout de la servlet de déconnexion

---
+++ src/services/DeconnexionServlet.java
@@ -0,0 +1,35 @@
+package services;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import mediatek2020.Mediatheque;
+import mediatek2020.items.Document;
+import mediatek2020.items.Utilisateur;
+
+public class DeconnexionServlet extends HttpServlet{
+
+	private static final long serialVersionUID = -1634389023286570897L;
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {   
+		PrintWriter out = response.getWriter();
+    	HttpSession session = request.getSession(false);
+       	
+		try  {
+			session.invalidate();
+			response.sendRedirect("./");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+}
