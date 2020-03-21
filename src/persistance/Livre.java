e0ab97c1c79dbd35d44feeb07e58d55b5fbc6e16
mallory94 <malloga@hotmail.fr>
2020-03-21 21:41:11 +0100

    suppression de Livre

--- src/persistance/Livre.java
+++
@@ -1,8 +0,0 @@
-package persistance;
-
-public class Livre extends DocumentEmpruntable {
-
-	public Livre(int idDocument, String NomDocument, String AuteurDocument, int IdUtilisateurEmprunteur) {
-		super(idDocument, NomDocument, AuteurDocument, IdUtilisateurEmprunteur);
-	}
-}
