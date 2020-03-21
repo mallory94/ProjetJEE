f7f8f09c333073f03f08086b96a33fc794849bc5
mallory94 <malloga@hotmail.fr>
2020-03-21 21:38:54 +0100

    prise en compte du type dans l'ajout de document et redirection vers l'accueil

--- src/services/AjouterDocumentServlet.java
+++ src/services/AjouterDocumentServlet.java
@@ -15,6 +15,8 @@ import mediatek2020.items.Utilisateur;
 
 public class AjouterDocumentServlet extends HttpServlet{
 
+	private static final long serialVersionUID = -7981051592619907988L;
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 		PrintWriter out = response.getWriter();
@@ -23,14 +25,19 @@ public class AjouterDocumentServlet extends HttpServlet{
     	Utilisateur user = (Utilisateur) session.getAttribute( "user" );
     	String nomDoc = request.getParameter("nomDoc");
     	String autDoc = request.getParameter("autDoc");
-       	
+    	String typeDoc = request.getParameter("typeDoc");
+       	System.out.println("typeDoc = " + typeDoc);
 		try  {
 			
 			Mediatheque.getInstance().nouveauDocument(0, 
-					new Object[] { nomDoc , autDoc} );
+					new Object[] { nomDoc , autDoc, typeDoc});
+			response.sendRedirect("./documentAjoute.jsp");
 		}
 		catch (Exception e) {
 			e.printStackTrace();
+			String message = "une erreur empechant l'ajout s'est produite";
+			session.setAttribute("erreur", message);
+			response.sendRedirect("./erreurBibliothecaire.jsp");
 		}
     }
 }
